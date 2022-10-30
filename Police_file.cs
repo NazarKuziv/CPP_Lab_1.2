236ffbf15a9dd6431802c5cb54ceaf45843bfeb1
unknown <nazar123kuziv@gmail.com>
2022-10-30 13:12:49 +0200

    first commit

---
+++ Police_file.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace CPP_Lab_1._2
+{
+
+    public class Police_file<T1, T2>
+    {
+        
+        public T1 Surname { get; set; }
+        public T1 Name { get; set; }
+        public T1 Middle_Name { get; set; }
+        public T2 Birthday { get; set; }
+        public T1[] Dates_of_Convictions { get; set; }
+        public T2 Date_of_Last_Imprisonment { get; set; }
+        public T2 Date_of_Last_Dismissal { get; set; }
+
+        static int tableWidth = 170;
+
+        public  List<Police_file<T1, T2>> Sort_by(List<Police_file<T1, T2>> prisonersList,int sort_by)
+        {
+            switch(sort_by)
+            {
+                case 1: return prisonersList.OrderBy(order => order.Surname).ToList();
+                case 2: return prisonersList.OrderBy(order => order.Birthday).ToList();
+                case 3: return prisonersList.OrderBy(order => order.Date_of_Last_Imprisonment).ToList();
+                case 4: return prisonersList.OrderBy(order => order.Date_of_Last_Dismissal).ToList();
+                default: return prisonersList;
+
+            }  
+        }
+
+
+        public void Display()
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+
+            if (Dates_of_Convictions.Length == 2)
+            {
+                string[] str = {Surname+" "+Name+" "+ Middle_Name,String.Format("{0:dd/MM/yyyy}", Birthday),
+                Dates_of_Convictions[0]+"-"+ Dates_of_Convictions[1],String.Format("{0:dd/MM/yyyy}",
+                Date_of_Last_Imprisonment),String.Format("{0:dd/MM/yyyy}", Date_of_Last_Dismissal)};
+
+                PrintRow(str);
+               
+            }
+            else
+            {
+                string[] str = {Surname+" "+Name+" "+ Middle_Name,String.Format("{0:dd/MM/yyyy}", Birthday),
+                Dates_of_Convictions[0]+"-"+ Dates_of_Convictions[1],String.Format("{0:dd/MM/yyyy}",
+                Date_of_Last_Imprisonment),String.Format("{0:dd/MM/yyyy}", Date_of_Last_Dismissal)};
+                PrintRow(str);
+
+                for(int i = 2; i < Dates_of_Convictions.Length-1;i++)
+                {
+                    PrintRow("","", Dates_of_Convictions[i] + "-" + Dates_of_Convictions[i+1], "","");
+                }
+
+                
+            }
+            PrintLine();
+
+
+
+        }
+        public static void PrintLine()
+        {
+            Console.WriteLine(new string('-', tableWidth));
+        }
+
+        public static void PrintRow(params string[] columns)
+        {
+            int width = (tableWidth - columns.Length) / columns.Length;
+            string row = "|";
+
+            foreach (var column in columns)
+            {
+                row += AlignCentre(column, width) + "|";
+            }
+
+            Console.WriteLine(row);
+        }
+
+        public static string AlignCentre(string text, int width)
+        {
+            text = text.Length > width ? text.Substring(0, width - 3) + "..." : text;
+
+            if (string.IsNullOrEmpty(text))
+            {
+                return new string(' ', width);
+            }
+            else
+            {
+                return text.PadRight(width - (width - text.Length) / 2).PadLeft(width);
+            }
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using static System.Collections.Specialized.BitVector32;
+
+namespace CPP_Lab_1._2
+{
+    public class Program
+    {
+
+        static void printTable(List<Police_file<string, DateTime>> list)
+        {
+            
+            Police_file<string, DateTime>.PrintLine();
+            Police_file<string, DateTime>.PrintRow("П.І.П", "Дата народження ", "Дати ув'язнень", "Дата ост ув'язнення", "Дата ост звільнення");
+            Police_file<string, DateTime>.PrintLine();
+            foreach (var item in list)
+            {
+                item.Display();
+            }
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.SetWindowSize(172, 50);
+            List<Police_file<string, DateTime>> prisoners = new List<Police_file<string, DateTime>>();
+
+            try
+            {
+                string filePfth = "..//..//police_file.txt";
+                List<string> lines = File.ReadAllLines(filePfth).ToList();
+
+                
+
+                foreach (var line in lines)
+                {
+                    string[] entries = line.Split(',');
+
+
+                    Police_file<string, DateTime> newPrisoner = new Police_file<string, DateTime>();
+                    newPrisoner.Surname = entries[0];
+                    newPrisoner.Name = entries[1];
+                    newPrisoner.Middle_Name = entries[2];
+                    newPrisoner.Birthday = Convert.ToDateTime(entries[3]);
+
+
+                    string[] date = new string[entries.Length-4];
+                    int i = 4, j = 0;
+                    
+                    while (i < entries.Length)
+                    { 
+                        date[j] = entries[i];
+                        i++;
+                        j++;
+                       
+                    }
+                    newPrisoner.Dates_of_Convictions = date;
+                    newPrisoner.Date_of_Last_Imprisonment = Convert.ToDateTime(date[j - 2]);
+                    newPrisoner.Date_of_Last_Dismissal = Convert.ToDateTime(date[j - 1]);
+
+
+                    prisoners.Add(newPrisoner);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            printTable(prisoners);
+                List<Police_file<string, DateTime>> sortedList = new List<Police_file<string, DateTime>>();
+
+                bool exit = false;
+                do
+                {
+                    int action = -1;
+                    Console.WriteLine(" Сортувати:");
+                    Console.WriteLine("  [1] - за Прізвищем");
+                    Console.WriteLine("  [2] -за Датаою народження");
+                    Console.WriteLine("  [3] -за Дата ост ув'язнення");
+                    Console.WriteLine("  [4] -за:Дата ост звільнення"); 
+                    Console.WriteLine("  [0] -Завершити роботу");
+                    Console.Write("  Виберіть дію: "); action = Convert.ToInt32(Console.ReadLine());
+                    switch (action)
+                    {
+                        case 1:
+                            {
+                                Console.Clear();
+                                sortedList = prisoners[0].Sort_by(prisoners, action);
+                                Console.WriteLine("\n Відсортовано за Прізвищем");
+                                printTable(sortedList);
+                                break;
+                            }
+                        case 2:
+                            {
+                                Console.Clear();
+                                sortedList = prisoners[0].Sort_by(prisoners, action);
+                                Console.WriteLine("\n Відсортовано за Датаою народження");
+                                printTable(sortedList);
+                                break;
+                            }
+                        case 3:
+                            {
+                                Console.Clear();
+                                sortedList = prisoners[0].Sort_by(prisoners, action);
+                                Console.WriteLine("\n Відсортовано за Дата ост ув'язнення");
+                                printTable(sortedList);
+                                break;
+                            }
+                        case 4:
+                            {
+                                Console.Clear();
+                                sortedList = prisoners[0].Sort_by(prisoners, action);
+                                Console.WriteLine("\n Відсортовано pf Дата ост звільнення");
+                                printTable(sortedList);
+                                break;
+                            }
+                        case 0:
+                            {
+                                exit = true;
+                                break;
+                            }
+                        default:
+                            {
+                                Console.Clear();
+                                Console.WriteLine("\nНевідома дія!");
+                                printTable(prisoners);
+                            break;
+                            }
+
+                    }
+
+
+                } while (exit == false);
+
+           
+
+
+
+        }
+    }
+}
